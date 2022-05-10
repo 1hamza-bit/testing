eaa105568e30b069ae161ee4970dd2ca5ae2a874
hamtah <hamtah112@gmail.com>
2022-05-10 17:59:36 +0500

    change

--- src/Components/videos/index.js
+++ src/Components/videos/index.js
@@ -2,22 +2,54 @@ import React, { useState } from 'react';
 import ItemsCarousel from 'react-items-carousel';
 import './video.css'
 import Card from  './card'
- 
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+
+import "slick-carousel/slick/slick-theme.css";
+
 const Items = () => {
   const [activeItemIndex, setActiveItemIndex] = useState(0);
   const chevronWidth = 40;
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
     <div style={{ padding: `5 ${chevronWidth}px`, padding:"5%" }}>
-      <ItemsCarousel
-        requestToChangeActive={setActiveItemIndex}
-        activeItemIndex={activeItemIndex}
-        numberOfCards={4}
-        gutter={20}
-        leftChevron={<button className='bit'>{'<'}</button>}
-        rightChevron={<button className='bit'>{'>'}</button>}
-        outsideChevron
-        chevronWidth={chevronWidth}
-      >
+     <Slider {...settings}>
         {/* card1  */}
         <div className="blog">
   
@@ -167,7 +199,7 @@ const Items = () => {
 <div className="color-overlay"></div>
 </div>
        
-      </ItemsCarousel>
+</Slider >
     </div>
   );
 };
--- src/Components/videos/vidcard.js
+++ src/Components/videos/vidcard.js
@@ -1,23 +1,57 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import ItemsCarousel from 'react-items-carousel';
 import './video.css'
 import Card from  './card'
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+
+import "slick-carousel/slick/slick-theme.css";
+
  
-const Item = () => {
-  const [activeItemIndex, setActiveItemIndex] = useState(0);
-  const chevronWidth = 40;
+class Item extends Component {
+
+  render() {
+    var settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+
   return (
-    <div style={{ padding: `5 ${chevronWidth}px`, padding:"5%" }}>
-      <ItemsCarousel
-        requestToChangeActive={setActiveItemIndex}
-        activeItemIndex={activeItemIndex}
-        numberOfCards={4}
-        gutter={20}
-        leftChevron={<button className='bit'>{'<'}</button>}
-        rightChevron={<button className='bit'>{'>'}</button>}
-        outsideChevron
-        chevronWidth={chevronWidth}
-      >
+   <div style={{padding: "5%"}}>
+       
+       
+      <Slider {...settings}>
         
 
         {/* card1  */}
@@ -37,8 +71,9 @@ const Item = () => {
 
 
        
-      </ItemsCarousel>
+        </Slider>
     </div>
   );
-};
+}
+}
 export default Item;
